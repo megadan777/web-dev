290ee2b366b4970baa5213c77accb3c4c82fdd11
Daniel Mowbray <daniel.mowbray86@gmail.com>
2023-09-08 08:49:11 +1000

    ex-obj0709

--- javascript-essential-training-2832077-main/Practice/03_07/script.js
+++ javascript-essential-training-2832077-main/Practice/03_07/script.js
@@ -7,3 +7,19 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bookshelf = {
+  name: "Bookshelf",
+  noOfBooks: 250,
+  shelves: 5,
+  color: "black",
+  numberOfBooksTopShelves: {
+    left: 47,
+    right: 30,
+  },
+};
+
+console.log("Black Bookshelf", bookshelf);
+
+var query = "numberOfBooksTopShelves";
+console.log("How many books are on the top shelves?", bookshelf[query]);
