1014cd2e3964a60c829af014f9fd20f01c3a0225
Daniel Mowbray <daniel.mowbray86@gmail.com>
2023-09-17 15:45:32 +1000

    1709-cart

--- Projects/Band-site/store.js
+++ Projects/Band-site/store.js
@@ -16,6 +16,12 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
+    
+    var addToCartButton = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButton.length; i++) {
+        var button = addToCartButton[i]
+        button.addEventListener('click', addToCartClicked)
+    }
 }
 
 function removeCartItem(event) {
@@ -30,6 +36,42 @@ function quantityChanged(event) {
         input.value = 1
     }
     updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if(cartItemNames[i].innerText == title) {
+            alert('This item has already been added')
+            return
+        }
+    }
+    var cartRowContents = `
+    <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" alt="Album 1">
+            <span class="cart-item-title">${title}</span>
+            </div>
+            <span class="cart-price cart-column">${price}</span>             
+            <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" role="button">REMOVE</button>
+        </div>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
 }
 
 function updateCartTotal() {
