d49926ca8aa86aeaef90171f2bb25306c7bf65f7
Daniel Mowbray <daniel.mowbray86@gmail.com>
2023-09-21 17:50:38 +1000

    2109-debug-activity

---
+++ Projects/debug.js
@@ -0,0 +1,17 @@
+ const users = [
+    { name: 'Kyle', age: 24 };
+    { name: 'Simon', age: 24 };
+    { name: 'Wendy', age: 34 };   
+    { name: 'Sally', age: 25 };
+    { name: 'Phil', };
+    { name: 'Mark', age: 28 };
+    { name: 'Colin', age: 45 };
+ ]
+
+users.forEach(user => {
+    printUser(user)
+})
+
+function.printUser(user)
+console.log('User ${user.name) will be ${user.age + 5} years old in 5 years.')
+}
