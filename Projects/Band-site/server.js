dd06823b3d595cb3aab6302ba4b318defda97f42
Daniel Mowbray <daniel.mowbray86@gmail.com>
2023-10-12 15:18:35 +1100

    1210

--- Projects/Band-site/server.js
+++ Projects/Band-site/server.js
@@ -1,3 +1,11 @@
+if (process.env.node_ENV !== 'production') {
+  require('dotenv').load()
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublicKey)
 
   const express = require('express')
   const app = express()
