3bee274256ae312a370ddcfb88130022cd38bb7c
Daniel Mowbray <daniel.mowbray86@gmail.com>
2023-10-24 21:26:49 +1100

    2410

--- Projects/Band-site/server.js
+++ Projects/Band-site/server.js
@@ -1,19 +1,32 @@
-if (process.env.node_ENV !== 'production') {
-  require('dotenv').load()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
-console.log(stripeSecretKey, stripePublicKey)
+console.log(stripeSecretKey, stripePublicKey);
 
-  const express = require('express')
-  const app = express()
-  
-  
-  app.set('view engine', 'ejs')
-  app.use(express.static('public'))
-  
-  app.get('/store')
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
-  app.listen(3000)
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/store', function(req, res) {
+  fs.readFile('items.json', function(err, data) {
+    if (err) {
+      res.status(500).end();
+    } else {
+      const items = JSON.parse(data);
+      res.render('store.ejs', {
+        items: items
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
